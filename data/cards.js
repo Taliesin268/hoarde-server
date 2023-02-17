667667f5b381af00fb53a056645fdf71ac8485e4
Adrien Rawsthorne <adester95@gmail.com>
2023-02-17 19:01:37 +1100

    Updated card list

--- data/cards.js
+++ data/cards.js
@@ -1,129 +1,372 @@
 module.exports = [
-    {
-        id: 1,
-        name: 'Aspect of Bahamut',
-        wager: 13,
-        type: 'Dragon',
-        alignments: 'Lawful Good',
-        rules_text: 'Lawful Good, unchangeable. Trumps unchangeable.',
-        image_url: 'http://localhost:4000/images/bahamut.png',
-        image_options: '{"object-position": "25% 75%"}'
-    },
-    {
-        id: 2,
-        name: 'Aspect of Tiamat',
-        wager: 13,
-        type: 'Dragon',
-        alignments: 'Chaotic Evil',
-        rules_text: 'Chaotic Evil, unchangeable. Trumps unchangeable.',
-        image_url: 'http://localhost:4000/images/tiamat.png'
-    },
-    {
-        id: 3,
-        name: 'Planetar',
-        wager: 7,
-        type: null,
-        alignments: 'Lawful Good',
-        rules_text: 'Lawful Good',
-        image_url: 'http://localhost:4000/images/Planetar.webp'
-    },
-    {
-        id: 4,
-        name: 'Pit Fiend',
-        wager: 7,
-        type: null,
-        alignments: 'Chaotic Evil',
-        rules_text: 'Chaotic Evil',
-        image_url: 'http://localhost:4000/images/pitfiend.webp'
-    },
-    {
-        id: 5,
-        name: 'Cloud Giant',
-        wager: 5,
-        type: null,
-        alignments: 'Good Evil',
-        rules_text: 'Good or Evil',
-        image_url: 'http://localhost:4000/images/cloudgiant.webp'
-    },
-    {
-        id: 6,
-        name: 'Assassin',
-        wager: 5,
-        type: null,
-        alignments: 'Lawful Chaotic',
-        rules_text: 'Lawful or Chaotic',
-        image_url: 'http://localhost:4000/images/assassin.png'
-    },
-    {
-        id: 7,
-        name: 'Displacer Beast',
-        wager: 4,
-        type: null,
-        alignments: 'Lawful Evil',
-        rules_text: 'Lawful or Evil',
-        image_url: 'http://localhost:4000/images/DisplacerBeast.webp'
-    },
-    {
-        id: 8,
-        name: 'Pegasus',
-        wager: 4,
-        type: null,
-        alignments: 'Chaotic Good',
-        rules_text: 'Chaotic or Good',
-        image_url: 'http://localhost:4000/images/Pegasus-5e.webp'
-    },
-    {
-        id: 9,
-        name: 'Couatl',
-        wager: 3,
-        type: null,
-        alignments: 'Lawful Good',
-        rules_text: 'Lawful or Good',
-        image_url: 'http://localhost:4000/images/couatl.png'
-    },
-    {
-        id: 10,
-        name: 'Gnoll',
-        wager: 3,
-        type: null,
-        alignments: 'Chaotic Evil',
-        rules_text: 'Chaotic or Evil',
-        image_url: 'http://localhost:4000/images/Gnoll-5e.webp'
-    },
-    {
-        id: 11,
-        name: 'Blink Pup',
-        wager: 2,
-        type: null,
-        alignments: 'Lawful Good',
-        rules_text: 'Lawful or Good',
-        image_url: 'http://localhost:4000/images/Blink_Dog.webp'
-    },
-    {
-        id: 12,
-        name: 'Flumph',
-        wager: 2,
-        type: null,
-        alignments: 'Lawful Good',
-        rules_text: 'Lawful or Good',
-        image_url: 'http://localhost:4000/images/Flumph.webp'
-    },
-    {
-        id: 13,
-        name: 'Goblin',
-        wager: 2,
-        type: null,
-        alignments: 'Chaotic Evil',
-        rules_text: 'Chaotic or Evil',
-        image_url: 'http://localhost:4000/images/Goblin.webp'
-    },
-    {
-        id: 14,
-        name: 'Specter',
-        wager: 2,
-        type: null,
-        alignments: 'Chaotic Evil',
-        rules_text: 'Chaotic or Evil',
-        image_url: 'http://localhost:4000/images/specter.jpg'
-    },
-];
+  {
+    name: "Quetzalcoatl",
+    wager: 13,
+    rules_text: "Lawful Good, unchangeable. Trumps unchangeable.",
+    alignments: "Lawful Good",
+    type: "Dragon",
+  },
+  {
+    name: "Tiamat",
+    wager: 13,
+    rules_text: "Chaotic Evil, unchangeable. Trumps unchangeable.",
+    alignments: "Chaotic Evil",
+    type: "Dragon",
+  },
+  {
+    name: "Kraken",
+    wager: 10,
+    rules_text: "Add 10 gold to your opponent's wager.",
+  },
+  {
+    name: "Cthulu?",
+    wager: 10,
+    rules_text:
+      "Your opponent reveals their hand next round and must play if able.",
+  },
+  {
+    name: "Dragon Turtle",
+    wager: 9,
+    rules_text: "Lawful. Discard a card to play this card again.",
+    alignments: "Lawful",
+    type: "Dragon",
+  },
+  {
+    name: "Lung Dragon",
+    wager: 9,
+    rules_text: "Good. Discard a card to play this card again.",
+    alignments: "Good",
+    type: "Dragon",
+  },
+  {
+    name: "UNNAMED",
+    wager: 9,
+    rules_text: "Chaotic. Discard a card to play this card again.",
+    alignments: "Chaotic",
+    type: "Dragon",
+  },
+  {
+    name: "Azhdaha?",
+    wager: 9,
+    rules_text: "Evil. Discard a card to play this card again.",
+    alignments: "Evil",
+    type: "Dragon",
+  },
+  {
+    name: "Angel",
+    wager: 8,
+    rules_text: "Lawful, unchangeable.",
+    alignments: "Lawful",
+  },
+  {
+    name: "Gryphon",
+    wager: 8,
+    rules_text: "Good, unchangeable.",
+    alignments: "Good",
+  },
+  {
+    name: "Demon",
+    wager: 8,
+    rules_text: "Chaotic, unchangeable.",
+    alignments: "Chaotic",
+  },
+  {
+    name: "Lich",
+    wager: 8,
+    rules_text: "Evil, unchangeable.",
+    alignments: "Evil",
+  },
+  {
+    name: "Thunderbird",
+    wager: 7,
+    rules_text: "Lawful Good",
+    alignments: "Lawful Good",
+  },
+  {
+    name: "Horned Serpent",
+    wager: 7,
+    rules_text: "Chaotic Evil",
+    alignments: "Chaotic Evil",
+  },
+  {
+    name: "Vampire",
+    wager: 7,
+    rules_text:
+      "Lawful Evil. If the opponent rests you may still change the alignment.",
+    alignments: "Lawful Evil",
+  },
+  {
+    name: "Werewolf",
+    wager: 7,
+    rules_text:
+      "Chaotic Good. If the opponent rests you may still change the alignment.",
+    alignments: "Chaotic Good",
+  },
+  {
+    name: "Kitsune",
+    wager: 7,
+    rules_text:
+      "Lawful or Good, or this may gain the alignment of your last played card.",
+    alignments: "Lawful Good",
+  },
+  {
+    name: 'Skin-Walker',
+    wager: 7,
+    rules_text: 'Chaotic or Evil, your next card may gain that alignment over its own.',
+    alignments: 'Chaotic Evil',
+    type: 'Dragon'
+  },
+  {
+    name: 'Naga',
+    wager: 6,
+    rules_text: 'Lawful. If the alignment is Good, you may halve or double the wagers.',
+    alignments: 'Lawful Good'
+  },
+  {
+    name: 'Treant',
+    wager: 6,
+    rules_text: 'Good. If the alignment is Chaotic, you may halve or double the wagers.',
+    alignments: 'Chaotic Good'
+  },
+  {
+    name: 'Manticore',
+    wager: 6,
+    rules_text: 'Chaotic. If the alignment is Evil, you may halve or double the wagers.',
+    alignments: 'Chaotic Evil'
+  },
+  {
+    name: 'Siren',
+    wager: 6,
+    rules_text: 'Evil. If the alignment is Lawful, you may halve or double the wagers.',
+    alignments: 'Lawful Evil'
+  },
+  {
+    name: 'Frost Giant',
+    wager: 6,
+    rules_text: 'Lawful. Trumps unchangeable, except for dragons.',
+    alignments: 'Lawful',
+  },
+  {
+    name: 'Storm Giant',
+    wager: 6,
+    rules_text: 'Good. Trumps unchangeable, except for dragons.',
+    alignments: 'Good',
+  },
+  {
+    name: 'Fire Giant',
+    wager: 6,
+    rules_text: 'Chaotic. Trumps unchangeable, except for dragons.',
+    alignments: 'Chaotic',
+  },
+  {
+    name: 'Mountain Giant',
+    wager: 6,
+    rules_text: 'Evil. Trumps unchangeable, except for dragons.',
+    alignments: 'Evil',
+  },
+  {
+    name: 'Djinn',
+    wager: 5,
+    rules_text: 'Good or Evil',
+    alignments: 'Good Evil'
+  },
+  {
+    name: 'Aqrabuamelu',
+    wager: 5,
+    rules_text: 'Lawful or Chaotic',
+    alignments: 'Lawful Chaotic'
+  },
+  {
+    name: 'Centaur',
+    wager: 4,
+    rules_text: 'Lawful. Pair with Gargoyle: Lawful Good, unchangeable.',
+    alignments: 'Lawful',
+  },
+  {
+    name: 'Unicorn',
+    wager: 4,
+    rules_text: 'Good. Pair with Mermaid: Lawful Good, unchangeable.',
+    alignments: 'Good',
+  },
+  {
+    name: 'Troll',
+    wager: 4,
+    rules_text: 'Chaotic. Pair with Goblin: Chaotic Evil, unchangeable.',
+    alignments: 'Chaotic',
+  },
+  {
+    name: 'Wraith',
+    wager: 4,
+    rules_text: 'Evil. Pair with Specter: Chaotic Evil, unchangeable.',
+    alignments: 'Evil',
+  },
+  {
+    name: "Banshee",
+    wager: 4,
+    rules_text: "Lawful or Evil",
+    alignments: "Lawful Evil",
+  },
+  {
+    name: "Pegasus",
+    wager: 4,
+    rules_text: "Chaotic or Good",
+    alignments: "Chaotic Good",
+  },
+  {
+    name: "Baku",
+    wager: 4,
+    rules_text: "Lawful if you've played a Lawful card, same for Good.",
+    alignments: "",
+  },
+  {
+    name: "Chimera",
+    wager: 4,
+    rules_text: "Chaotic if you've played a Chaotic card, same for Evil.",
+    alignments: "",
+  },
+  {
+    name: "Coatl",
+    wager: 3,
+    rules_text: "Lawful or Good",
+    alignments: "Lawful Good",
+  },
+  {
+    name: "Harpy",
+    wager: 3,
+    rules_text: "Chaotic or Evil",
+    alignments: "Chaotic Evil",
+  },
+  {
+    name: "Skeleton",
+    wager: 2,
+    rules_text: "Lawful. Free to play.",
+    alignments: "Lawful",
+  },
+  {
+    name: "Sprite",
+    wager: 2,
+    rules_text: "Good. Free to play.",
+    alignments: "Good",
+  },
+  {
+    name: "Imp",
+    wager: 2,
+    rules_text: "Chaotic. Free to play.",
+    alignments: "Chaotic",
+  },
+  {
+    name: "Ghoul",
+    wager: 2,
+    rules_text: "Evil. Free to play.",
+    alignments: "Evil",
+  },
+  {
+    name: "Gargoyle",
+    wager: 2,
+    rules_text: "Lawful or Good",
+    alignments: "Lawful Good",
+  },
+  {
+    name: "Mermaid",
+    wager: 2,
+    rules_text: "Lawful or Good",
+    alignments: "Lawful Good",
+  },
+  {
+    name: "Goblin",
+    wager: 2,
+    rules_text: "Chaotic or Evil",
+    alignments: "Chaotic Evil",
+  },
+  {
+    name: "Specter",
+    wager: 2,
+    rules_text: "Chaotic or Evil",
+    alignments: "Chaotic Evil",
+  },
+  {
+    name: "Wendigo",
+    wager: 5,
+    rules_text: "Exchange wager amounts. Free to play.",
+    alignments: "",
+  },
+  {
+    name: "Sphinx",
+    wager: 5,
+    rules_text: "Reverse the effect/alignment of each card played this round. Free to play.",
+    alignments: "",
+  },
+  {
+    name: "Phoenix",
+    wager: 5,
+    rules_text: "Replay another card. Discard a card to play this card again.",
+  },
+  {
+    name: "Hydra",
+    wager: 5,
+    rules_text: "If your opponent played: 1 card, Lawful. 2, Good. 3, Chaotic. 4+, Evil.",
+    alignments: "Lawful Good Chaotic Evil",
+  },
+  {
+    name: "Golem",
+    wager: 5,
+    rules_text: "Lawful, Chaotic, Good or Evil.",
+    alignments: "Lawful Chaotic Good Evil",
+  },
+  {
+    name: "Raijū",
+    wager: 5,
+    rules_text: "Play immediately on draw, the opponent discards a random card.",
+    alignments: "",
+  },
+  {
+    name: "Rainbow Serpent",
+    wager: 3,
+    rules_text: "True Neutral, unchangeable. [Wagers remain into next round]",
+    alignments: "",
+    type: "Dragon"
+  },
+  {
+    name: "Wyvern",
+    wager: 3,
+    rules_text: "Copy the Whelp or a Dragon you played this round.",
+    alignments: "",
+    type: "Dragon"
+  },
+  {
+    name: "Gorgon",
+    wager: 3,
+    rules_text: "If your last card had an alignment, it's unchangeable. Free to play.",
+    alignments: "",
+  },
+  {
+    name: "Pontianak",
+    wager: 3,
+    rules_text: "Your opponent discards a card. Free to play.",
+    alignments: "",
+  },
+  {
+    name: "Yeti",
+    wager: 3,
+    rules_text: "If you have wagered the most, gain 5 gold from your opponent.",
+    alignments: "",
+  },
+  {
+    name: "Basilisk",
+    wager: 3,
+    rules_text: "The next card played won't change the alignment. Free to play.",
+    alignments: "",
+  },
+  {
+    name: "Slime",
+    wager: 1,
+    rules_text: "Remove a card, its effect and its gold from the round (not alignment).",
+    alignments: "",
+  },
+  {
+    name: "Mimic",
+    wager: 1,
+    rules_text: "Copy the last card you or your opponent played.",
+    alignments: "",
+  },
+]
